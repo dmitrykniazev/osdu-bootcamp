22b2901d6781b9151a0e9997ef123f1e9fa7584d
Dmitry Kniazev <dmitry_kniazev@epam.com>
2020-04-21 22:33:57 -0500

    Update setup.ps1

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,20 +1,20 @@
 # Install Chocolatey
-#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-#Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Assign Chocolatey Packages to Install
-#$Packages = `
-#  'git', `
-#  'googlechrome', `
-#  'visualstudiocode', `
-#  'docker-desktop', `
-#  'postman', `  
-#  'microsoft-windows-terminal', `
-#  'cascadiacode'
+$Packages = `
+  'git', `
+  'googlechrome', `
+  'visualstudiocode', `
+  'docker-desktop', `
+  'postman', `  
+  'microsoft-windows-terminal', `
+  'cascadiacode'
 
 # Install Packages
-#ForEach ($PackageName in $Packages)
-#{ choco install $PackageName -y }
+ForEach ($PackageName in $Packages)
+{ choco install $PackageName -y }
 
 # Add User to Docker Group
 Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
@@ -23,11 +23,11 @@ Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 
 # Download and Install Debian
-#Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile ~/Debian.appx -UseBasicParsing
-#Add-AppxPackage -Path ~/Debian.appx
+Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile ~/Debian.appx -UseBasicParsing
+Add-AppxPackage -Path ~/Debian.appx
 
 # Configure Terminal
-# Invoke-WebRequest -Uri https://github.com/dmitrykniazev/osdu-bootcamp/blob/master/scripts/settings.json -OutFile $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -UseBasicParsing
+Invoke-WebRequest -Uri https://github.com/dmitrykniazev/osdu-bootcamp/blob/master/scripts/settings.json -OutFile $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -UseBasicParsing
 
 # Reboot
-#Restart-Computer
+Restart-Computer
