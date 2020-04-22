8e140c89d6bd518bb16574ab3642e55fdac646c5
Dmitry Kniazev [EPAM] <dmitry_kniazev@epam.com>
2020-04-21 21:32:26 -0500

    Add files via upload

---
+++ scripts/setup.ps1
@@ -0,0 +1,33 @@
+# Install Chocolatey
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Assign Chocolatey Packages to Install
+$Packages = `
+  'git', `
+  'googlechrome', `
+  'visualstudiocode', `
+  'docker-desktop', `
+  'postman', `  
+  'microsoft-windows-terminal' `
+  'cascadiacode',
+
+# Install Packages
+ForEach ($PackageName in $Packages)
+{ choco install $PackageName -y }
+
+# Add User to Docker Group
+Add-LocalGroupMember -Group "docker-users" -Member "azureuser"
+
+# Enable WSL
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+# Download and Install Debian
+Invoke-WebRequest -Uri https://aka.ms/wsl-debian-gnulinux -OutFile ~/Debian.appx -UseBasicParsing
+Add-AppxPackage -Path ~/Debian.appx
+
+# Configure Terminal
+Invoke-WebRequest -Uri https://community.opengroup.org/osdu/program/-/blob/master/App_Dev_Training/Bootcamps/scripts/settings.json -OutFile $env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json -UseBasicParsing
+
+# Reboot
+Restart-Computer
---
+++ scripts/setup.sh
@@ -0,0 +1,27 @@
+echo "Updating "
+sudo apt update
+sudo apt install -y software-properties-common python3 python3-venv python3-pip
+apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt install -y vim git wget unzip
+
+# Download and install the Microsoft signing key
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
+# Add the Azure CLI software repository
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+# Update repository information and install the azure-cli package
+apt-get update
+apt-get install azure-cli -y
+
+# Install Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
+echo 'export DOCKER_HOST=tcp://localhost:2375' >> ~/.bashrc
